cf0d35937366797ebee5cd706f6d151233b3fc16
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2015-01-13 19:15:25 -0200

    add maven to path on linux

--- paths.sh
+++ paths.sh
@@ -2,7 +2,10 @@ export JENV_HOME=$HOME/.jenv
 
 if [[ "$OSTYPE" = *linux* ]] then
    export SCALA_PATH=$HOME/Programs/scala
+	 export M2_HOME=$HOME/Programs/apache-maven
+
    export PATH=$SCALA_PATH/bin:$PATH
+	 export PATH=$M2_PATH/bin:$PATH
 fi
 
 export PATH=/usr/local/bin:$PATH
