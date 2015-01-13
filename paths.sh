ac21a0c45e45476e6d4d6e13fc81da0bc20d1920
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2015-01-13 11:11:12 -0200

    add scala path to linux environment

--- paths.sh
+++ paths.sh
@@ -1,5 +1,10 @@
 export JENV_HOME=$HOME/.jenv
 
+if [[ "$OSTYPE" = *linux* ]] then
+   export SCALA_PATH=$HOME/Programs/scala
+   export PATH=$SCALA_PATH/bin:$PATH
+fi
+
 export PATH=/usr/local/bin:$PATH
 export PATH=$HOME/.rvm/bin:$PATH
 export PATH=$JENV_HOME/bin:$PATH
