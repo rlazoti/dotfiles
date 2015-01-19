9a96ebda0566f8f97c42d7b16bf7d2c224ea7959
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2015-01-19 11:47:21 -0200

    add tmux-powerline theme

--- install.zsh
+++ install.zsh
@@ -8,9 +8,10 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # set hidden files
 echo "install hidden files..."
-ln -s .terminal/zshenv ~/.zshenv
-ln -s .terminal/zshrc ~/.zshrc
-ln -s .terminal/tmux.conf ~/.tmux.conf
-ln -s .terminal/my-custom-theme.zsh-theme ~/.oh-my-zsh/themes/my-custom-theme.zsh-theme
-
+ln -s ~/.terminal/zshenv ~/.zshenv
+ln -s ~/.terminal/zshrc ~/.zshrc
+ln -s ~/.terminal/tmux.conf ~/.tmux.conf
+ln -s ~/.terminal/my-custom-theme.zsh-theme ~/.oh-my-zsh/themes/my-custom-theme.zsh-theme
+mv ~/.terminal/tmux-powerline/themes/default.sh ~/.terminal/tmux-powerline/themes/original-theme.sh
+ln -s ~/.terminal/my-powerline-theme.zsh-theme ~/.terminal/tmux-powerline/themes/default.zh
 echo "done. Have fun! :)"
