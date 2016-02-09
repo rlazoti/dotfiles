9ee7befe45f51bd531e29bdefbd8f6ead12a2f4a
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2016-02-09 10:01:19 -0200

    change paths for both OS

--- paths.sh
+++ paths.sh
@@ -1,16 +1,18 @@
-export JENV_HOME=$HOME/.jenv
-
+# linux
 if [[ "$OSTYPE" = *linux* ]] then
    export SCALA_PATH=$HOME/Programs/scala
-	 export M2_HOME=$HOME/Programs/apache-maven
+   export M2_HOME=$HOME/Programs/apache-maven
 
    export PATH=$SCALA_PATH/bin:$PATH
-	 export PATH=$M2_HOME/bin:$PATH
+   export PATH=$M2_HOME/bin:$PATH
+	 export PATH=$HOME/.rvm/bin:$PATH
+
+# macos
+else
+		export PATH=$HOME/.rbenv/shims:$PATH
 fi
 
+# shared
 export EDITOR="emacs -nw --no-splash"
-
 export PATH=/usr/local/bin:$PATH
-export PATH=$HOME/.rvm/bin:$PATH
-export PATH=$JENV_HOME/bin:$PATH
 export PATH=$PATH:~/bin
