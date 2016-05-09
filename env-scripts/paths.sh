5a0e457780ba27eebcefc552c0e51a4c046ec37f
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2016-05-09 18:32:34 -0300

    add gradle and flink to path

--- env-scripts/paths.sh
+++ env-scripts/paths.sh
@@ -3,11 +3,15 @@ if [[ "$OSTYPE" = *linux* ]] then
    export SCALA_PATH=$HOME/Programs/scala
    export M2_HOME=$HOME/Programs/apache-maven
    export SWIFT_PATH=$HOME/Programs/swift/usr
+	 export GRADLE_PATH=$HOME/Programs/gradle
+	 export FLINK_HOME=$HOME/Programs/flink
 
+	 export PATH=$FLINK_HOME/bin:$PATH
 	 export PATH=$SWIFT_PATH/bin:$PATH
    export PATH=$SCALA_PATH/bin:$PATH
    export PATH=$M2_HOME/bin:$PATH
 	 export PATH=$HOME/.rvm/bin:$PATH
+	 export PATH=$GRADLE_PATH/bin:$PATH
 
 # macos
 else
