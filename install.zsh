50f6759a81641d8eb40fe4c54a7e53f24f842530
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2016-06-04 18:05:54 -0300

    improve install.sh

--- install.zsh
+++ install.zsh
@@ -1,16 +1,23 @@
 # clone tmux-powerline
 echo "install tmux-powerline..."
+rm -rf ~/.terminal/tmux-powerline
 git clone https://github.com/erikw/tmux-powerline.git ~/.terminal/tmux-powerline
 
 # clone oh-my-zsh
 echo "install oh-my-zsh"
+rm -rf ~/.oh-my-zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # define global git commit msg template
 git config --global commit.template ~/.terminal/git_commit_msg.txt
 
 # set hidden files
-echo "install hidden files..."
+echo "remove old conf files..."
+rm ~/.zshenv
+rm ~/.zshrc
+rm ~/.tmux.conf
+
+echo "install conf files..."
 ln -s ~/.terminal/zshenv ~/.zshenv
 ln -s ~/.terminal/zshrc ~/.zshrc
 ln -s ~/.terminal/tmux.conf ~/.tmux.conf
