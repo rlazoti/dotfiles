c70b1c114d1640f3606e8b2b2481a8d35e7ab882
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2016-07-31 10:22:06 -0300

    fix brew sbin's path

--- env-scripts/paths.sh
+++ env-scripts/paths.sh
@@ -3,19 +3,20 @@ if [[ "$OSTYPE" = *linux* ]] then
    export SCALA_PATH=$HOME/Programs/scala
    export M2_HOME=$HOME/Programs/apache-maven
    export SWIFT_PATH=$HOME/Programs/swift/usr
-	 export GRADLE_PATH=$HOME/Programs/gradle
-	 export FLINK_HOME=$HOME/Programs/flink
+   export GRADLE_PATH=$HOME/Programs/gradle
+   export FLINK_HOME=$HOME/Programs/flink
 
-	 export PATH=$FLINK_HOME/bin:$PATH
-	 export PATH=$SWIFT_PATH/bin:$PATH
+   export PATH=$FLINK_HOME/bin:$PATH
+   export PATH=$SWIFT_PATH/bin:$PATH
    export PATH=$SCALA_PATH/bin:$PATH
    export PATH=$M2_HOME/bin:$PATH
-	 export PATH=$HOME/.rvm/bin:$PATH
-	 export PATH=$GRADLE_PATH/bin:$PATH
+   export PATH=$HOME/.rvm/bin:$PATH
+   export PATH=$GRADLE_PATH/bin:$PATH
 
 # macos
 else
-		export PATH=$HOME/.rbenv/shims:$PATH
+    export PATH=$HOME/.rbenv/shims:$PATH
+    export PATH=/usr/local/sbin:$PATH
 fi
 
 # shared
