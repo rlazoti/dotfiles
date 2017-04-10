9370aa68f820a4043ee19a6a4428d8429a9ddb70
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2017-04-10 14:38:18 -0300

    add terminal emacs alias

--- env-scripts/aliases.sh
+++ env-scripts/aliases.sh
@@ -29,7 +29,7 @@ alias -g hosts="sudo XMODIFIERS=\'\' vi /etc/hosts"
 alias -g n="node"
 alias -g E="sudo $EDITOR "
 alias -g ii="XMODIFIERS=\'\' emacs -nw --no-splash"
-alias -g vi="XMODIFIERS=\'\' emacs -nw --no-splash --no-init-file"
+alias -g vi="XMODIFIERS=\'\' emacs -nw --no-splash -q -l \"~/.emacs.d/terminal/init.el\""
 alias -g sk9='sudo kill -9'
 alias -g psg='ps aux | grep'
 alias -g ee="open -n -a /Applications/Emacs.app $1"
