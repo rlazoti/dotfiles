f07ec2e507593be5e5b57f24266eacaef341cdcb
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2017-08-28 12:50:38 -0300

    improve brew packages

--- osx-setup.sh
+++ osx-setup.sh
@@ -1,19 +1,25 @@
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+
+# essentinal packages
 brew install curl
 brew install git
 brew install zsh
+brew install zsh-completions
 brew install tmux
 brew install reattach-to-user-namespace
 
 
+# general packages
 brew install aspell
 brew install emacs-mac
 brew install giter8
 brew install htop
 brew install ispell
+brew install jq
 brew install leiningen
+brew install m-cli
 brew install maven
 brew install pstree
 brew install sbcl
@@ -22,8 +28,7 @@ brew install scala
 brew install the_silver_searcher
 brew install tree
 brew install unar
+brew install unrar
+brew install wget
 brew install wrk
-
-brew install giter8
-brew install sbt
-brew install scala
+brew install xz
