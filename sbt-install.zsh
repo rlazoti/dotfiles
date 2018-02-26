ddf399ad6baaa3631b78a5d9e50f371e61951ac9
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2018-02-26 10:30:09 -0300

    change sbt install script to use sbt 1.0

--- sbt-install.zsh
+++ sbt-install.zsh
@@ -1,9 +1,9 @@
 echo "remove old sbt files..."
-rm -rf ~/.sbt/0.13/plugins
-rm ~/.sbt/0.13/global.sbt
+rm -rf ~/.sbt/1.0/plugins
+rm ~/.sbt/1.0/global.sbt
 
 echo "install sbt files..."
-ln -s ~/.terminal/sbt/plugins ~/.sbt/0.13/plugins
-ln -s ~/.terminal/sbt/global.sbt ~/.sbt/0.13/global.sbt
+ln -s ~/.terminal/sbt/plugins ~/.sbt/1.0/plugins
+ln -s ~/.terminal/sbt/global.sbt ~/.sbt/1.0/global.sbt
 
 echo "done. Have fun! :)"
