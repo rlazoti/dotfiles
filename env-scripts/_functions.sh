ae2de02ba31711e03aea7e8179a2f26237ed3bc1
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2019-07-18 11:39:02 -0500

    master: Add func to reset git branch

--- env-scripts/_functions.sh
+++ env-scripts/_functions.sh
@@ -1,12 +1,10 @@
 # Convert unix epoch to date time
 function epoch_to_date() {
-    ##value=$(($1 / 1000000000000))
-
     if [[ $(($1 / 1000000000000)) -ge 1 ]]
     then
         date -j -f %s "$(expr $1 / 1000)"
     else
-	date -j -f %s $1
+        date -j -f %s $1
     fi
 }
 
@@ -38,6 +36,64 @@ function gplb() {
 # Git push with current local branch name
 function gpsb() {
     git push $1 origin $(git symbolic-ref --short HEAD)
+}
+
+# Drop local branch and pull it again from origin
+function git_reset_branch() {
+    CURRENT_DIR=$(pwd)
+
+    if [ -z "$1" ];
+    then
+        echo ERROR: No branch was given. Please enter a branch name 1>&2
+        echo git_reset_branch develop
+    else
+
+        for DIR in $(find . -type d -name .git)
+        do
+            if [[ "${DIR}" != *".terraform/modules/"* ]];
+            then
+                cd $DIR/../
+                echo "INFO: Reseting branch '$1' on $(pwd)"
+
+                if [[ -z $(git status -s) ]];
+                then
+                    if git show-ref --verify --quiet refs/heads/$1
+                    then
+
+                        if git show-ref --verify --quiet refs/heads/staging
+                        then
+                            git checkout --quiet staging
+                            git branch --quiet -D $1
+                            git prune
+                            git fetch --quiet
+                            git checkout --quiet $1
+                            git pull origin --quiet --rebase --tags $1
+                            git checkout --quiet staging
+                        else
+                            git checkout --quiet master
+                            git branch --quiet -D $1
+                            git prune
+                            git fetch --quiet
+                            git checkout --quiet $1
+                            git pull origin --quiet --rebase --tags $1
+                            git checkout --quiet master
+                        fi
+                        echo "INFO: OK $(pwd)"
+
+                    else
+                        echo "WARNING: branch '$1' not found."
+                    fi
+
+                else
+                    echo "WARNING: $(pwd) has unstaged changes."
+                fi
+
+                cd $CURRENT_DIR
+            fi
+        done
+
+        cd $CURRENT_DIR
+    fi
 }
 
 # Run git pull to update all Git repositories in a working directory and its subdirectories
