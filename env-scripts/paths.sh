8f03d1e378fc31c24c97a61ca29edb1a46df41ac
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2019-10-14 11:38:01 -0500

    master: Add gopath env var

--- env-scripts/paths.sh
+++ env-scripts/paths.sh
@@ -23,6 +23,8 @@ else
 fi
 
 # shared
+export GOPATH=$HOME/Projects/Go
 export EDITOR="emacs -nw --no-splash"
 export PATH=/usr/local/bin:$PATH
 export PATH=$PATH:~/.bin
+export PATH=$PATH:$GOPATH/bin
